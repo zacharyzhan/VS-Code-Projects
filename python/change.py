911b1e09302dd3f6ef4e52a6073c13db7cc7b109
zachary <zacharyzhan@foxmail.com>
2024-10-17 09:11:58 +0800

    main

---
+++ python/change.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# 读取CSV文件
+df = pd.read_csv(r"C:\Users\zacha\Desktop\cla_pre_vit_base_r50_s16_384.csv", header=None)
+
+# 假设第二列的名称是 'column_2'，将 0 和 1 互换
+df.iloc[:, 1] = df.iloc[:, 1].apply(lambda x: 1 if x == 0 else 0)
+
+# 保存修改后的CSV文件
+df.to_csv('output_file.csv', index=False, header=False, encoding='utf-8')
+
+print("output_file.csv")
